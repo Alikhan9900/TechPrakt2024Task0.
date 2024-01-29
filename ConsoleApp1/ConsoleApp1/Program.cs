bf796fc893fb0746fbb4dd3dd19f064e46218887
Аліхан Бекіров <alikhanbekirov@gmail.com>
2024-01-29 19:28:41 +0200

    add consoleapp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть перше число:");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("Введіть друге число:");
+            string input2 = Console.ReadLine();
+
+            if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+            {
+                double sum = number1 + number2;
+                Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Помилка введення. Будь ласка, введіть коректні числа.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
